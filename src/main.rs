7fedf3f3ec46051034c9f2965544aacb38e08a6c
Maciej Mikulski <maciej.mikulski@sparklab.pl>
2023-01-28 17:20:09 +0100

    Padlles and collisions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use ggez::{
     event,
-    graphics::{self, Color},
+    graphics::{self, Canvas, Color},
     input::keyboard::{KeyCode, KeyInput},
     Context, GameResult,
 };
 
-const SCREEN_SIZE: (f32, f32) = (800.0, 600.0);
+const SCREEN_SIZE: (f32, f32) = (800., 600.);
+const PADDLE_SIZE: (f32, f32) = (25., 150.);
 
 struct Label {
     text: String,
@@ -14,7 +15,7 @@ struct Label {
 impl Label {
     pub fn new() -> Self {
         Label {
-            text: String::from("Hello"),
+            text: String::from("Hello Pong!"),
         }
     }
 }
@@ -25,76 +26,100 @@ struct Rectangle {
     w: f32,
     h: f32,
     direction: Direction,
+    speed: f32,
+    autorun: bool,
 }
 
 impl Rectangle {
-    pub fn new() -> Self {
+    pub fn new(x: f32, y: f32, w: f32, h: f32, speed: f32, autorun: bool) -> Self {
         Rectangle {
-            x: 123.0,
-            y: 123.0,
-            w: 10.0,
-            h: 10.0,
-            direction: Direction::Up,
+            x,
+            y,
+            w,
+            h,
+            direction: Direction::Right,
+            speed,
+            autorun,
         }
     }
 
-    pub fn update(&mut self) -> GameResult {
+    pub fn update(&mut self) -> () {
+        self.collide();
+
+        if self.autorun {
+            match self.direction {
+                Direction::Down => self.move_down(),
+                Direction::Up => self.move_up(),
+                Direction::Right => self.move_right(),
+                Direction::Left => self.move_left(),
+            }
+        }
+    }
+
+    fn collide(&mut self) -> () {
         match self.direction {
-            Direction::Down => self.move_down(),
-            Direction::Up => self.move_up(),
-            Direction::Right => self.move_right(),
-            Direction::Left => self.move_left(),
+            Direction::Down => {
+                if self.y >= SCREEN_SIZE.1 {
+                    println!("{}", "changing direction");
+                    self.direction = Direction::inverse(&self.direction);
+                }
+            }
+            Direction::Up => {
+                if self.y <= 0. {
+                    println!("{}", "changing direction");
+                    self.direction = Direction::inverse(&self.direction);
+                }
+            }
+            Direction::Right => {
+                if self.x >= SCREEN_SIZE.0 {
+                    println!("{}", "changing direction");
+                    self.direction = Direction::inverse(&self.direction);
+                }
+            }
+            Direction::Left => {
+                if self.x <= 0. {
+                    println!("{}", "changing direction");
+                    self.direction = Direction::inverse(&self.direction);
+                }
+            }
         }
+    }
+
+    pub fn move_right(&mut self) -> () {
+        println!("right {} towards {}", self.x, SCREEN_SIZE.0);
+        if self.x < SCREEN_SIZE.0 {
+            self.x += self.speed;
+        }
+    }
+    pub fn move_left(&mut self) -> () {
+        println!("left {} from {}", self.x, SCREEN_SIZE.0);
+        if self.x > 0. {
+            self.x -= self.speed;
+        }
+    }
+    pub fn move_up(&mut self) -> () {
+        println!("up {} from {}", self.y, SCREEN_SIZE.1);
+        if self.y > 0. {
+            self.y -= self.speed;
+        }
+    }
+    pub fn move_down(&mut self) -> () {
+        println!("down {} towards {}", self.y, SCREEN_SIZE.1);
+        if self.y < SCREEN_SIZE.1 {
+            self.y += self.speed;
+        }
+    }
+
+    pub fn draw(&self, ctx: &Context, canvas: &mut Canvas) -> GameResult {
+        let rect = graphics::Rect::new(self.x, self.y, self.w, self.h);
+        let rect_to_draw = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            rect,
+            Color::from([0.2, 0.3, 0.4, 1.0]),
+        )?;
+        canvas.draw(&rect_to_draw, ggez::glam::Vec2::new(self.x, self.y));
         Ok(())
-    }
-
-    fn move_right(&mut self) -> () {
-        println!("{}", self.x);
-        println!("{}", SCREEN_SIZE.0);
-        // println!("{}", SCREEN_SIZE.0 == self.x);
-        if self.x == SCREEN_SIZE.0 {
-            println!("{}", "changing direction");
-            self.direction = Direction::Left;
-        } else {
-            self.x += 1.0;
-        }
-        // Ok(())
-    }
-    fn move_left(&mut self) -> () {
-        println!("{}", self.x);
-        println!("{}", SCREEN_SIZE.0);
-        // println!("{}", SCREEN_SIZE.0 == self.x);
-        if self.x == 0. {
-            println!("{}", "changing direction");
-            self.direction = Direction::Right;
-        } else {
-            self.x -= 1.0;
-        }
-        // Ok(())
-    }
-    fn move_up(&mut self) -> () {
-        println!("{}", self.y);
-        println!("{}", SCREEN_SIZE.1);
-        // println!("{}", SCREEN_SIZE.0 == self.x);
-        if self.y == 0. {
-            println!("{}", "changing direction");
-            self.direction = Direction::Down;
-        } else {
-            self.y -= 1.0;
-        }
-        // Ok(())
-    }
-    fn move_down(&mut self) -> () {
-        println!("{}", self.y);
-        println!("{}", SCREEN_SIZE.1);
-
-        if self.y == SCREEN_SIZE.1 {
-            println!("{}", "changing direction");
-            self.direction = Direction::Up;
-        } else {
-            self.y += 1.0;
-        }
-        // Ok(())
     }
 }
 
@@ -107,22 +132,15 @@ enum Direction {
 }
 
 impl Direction {
-    // /// We create a helper function that will allow us to easily get the inverse
-    // /// of a `Direction` which we can use later to check if the player should be
-    // /// able to move the snake in a certain direction.
-    // pub fn inverse(&self) -> Self {
-    //     match *self {
-    //         Direction::Up => Direction::Down,
-    //         Direction::Down => Direction::Up,
-    //         Direction::Left => Direction::Right,
-    //         Direction::Right => Direction::Left,
-    //     }
-    // }
+    pub fn inverse(&self) -> Self {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
 
-    /// We also create a helper function that will let us convert between a
-    /// `ggez` `Keycode` and the `Direction` that it represents. Of course,
-    /// not every keycode represents a direction, so we return `None` if this
-    /// is the case.
     pub fn from_keycode(key: KeyCode) -> Option<Direction> {
         match key {
             KeyCode::Up => Some(Direction::Up),
@@ -136,92 +154,89 @@ impl Direction {
 
 struct GameState {
     label: Label,
-    rectangle: Rectangle,
+    ball: Rectangle,
+    paddle_left: Rectangle,
+    paddle_right: Rectangle,
 }
 
 impl GameState {
     pub fn new() -> Self {
         GameState {
             label: Label::new(),
-            rectangle: Rectangle::new(),
+            ball: Rectangle::new(123., 123., 10., 10., 4., true),
+            paddle_left: Rectangle::new(
+                0. + PADDLE_SIZE.0 / 2.,
+                SCREEN_SIZE.1 / 2.,
+                PADDLE_SIZE.0,
+                PADDLE_SIZE.1,
+                2.,
+                false,
+            ),
+            paddle_right: Rectangle::new(
+                SCREEN_SIZE.0 - PADDLE_SIZE.0,
+                SCREEN_SIZE.1 / 2.,
+                PADDLE_SIZE.0,
+                PADDLE_SIZE.1,
+                2.,
+                false,
+            ),
         }
     }
 }
 
 impl event::EventHandler<ggez::GameError> for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        // self.label.text = String::from("xyz");
-        // self.rectangle.w += 1.0;
-        self.rectangle.update();
+        let pressed_keys = ctx.keyboard.pressed_keys();
+        for key in pressed_keys {
+            match key {
+                KeyCode::W => self.paddle_left.move_up(),
+                KeyCode::S => self.paddle_left.move_down(),
+                KeyCode::Up => self.paddle_right.move_up(),
+                KeyCode::Down => self.paddle_right.move_down(),
+                _ => (),
+            }
+        }
 
-        // if ctx.keyboard.is_key_pressed(KeyCode::Up) {
-        //     self.rectangle.y -= 1.0;
-        // }
-        // if ctx.keyboard.is_key_pressed(KeyCode::Down) {
-        //     self.rectangle.y += 1.0;
-        // }
-        // if ctx.keyboard.is_key_pressed(KeyCode::Left) {
-        //     self.rectangle.x -= 1.0;
-        // }
-        // if ctx.keyboard.is_key_pressed(KeyCode::Right) {
-        //     self.rectangle.x += 1.0;
-        // }
+        let ball_rect = graphics::Rect::new(self.ball.x, self.ball.y, self.ball.w, self.ball.h);
+        let paddle_left_rect = graphics::Rect::new(self.paddle_left.x, self.paddle_left.y, self.paddle_left.w, self.paddle_left.h);
+        let paddle_right_rect = graphics::Rect::new(self.paddle_right.x, self.paddle_right.y, self.paddle_right.w, self.paddle_right.h);
+
+        if ball_rect.overlaps(&paddle_left_rect) || ball_rect.overlaps(&paddle_right_rect) {
+            self.ball.direction = self.ball.direction.inverse();
+        }
+
+        self.ball.update();
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        // print!("{}", self.label.text);
+        let mut canvas = Canvas::from_frame(ctx, graphics::Color::RED);
 
-        let mut canvas = graphics::Canvas::from_frame(ctx, graphics::Color::RED);
-
-        // Text is drawn from the top-left corner.
         let dest_point = ggez::glam::Vec2::new(10.0, 10.0);
         canvas.draw(
             graphics::Text::new(&self.label.text).set_scale(48.),
             dest_point,
         );
 
-        let rect = graphics::Rect::new(
-            self.rectangle.x,
-            self.rectangle.y,
-            self.rectangle.w,
-            self.rectangle.h,
-        );
-        let rect_to_draw = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            rect,
-            graphics::Color::from([0.2, 0.3, 0.4, 1.0]),
-        )?;
-        canvas.draw(
-            &rect_to_draw,
-            ggez::glam::Vec2::new(self.rectangle.x, self.rectangle.y),
-        );
+        self.ball.draw(ctx, &mut canvas)?;
+        self.paddle_left.draw(ctx, &mut canvas)?;
+        self.paddle_right.draw(ctx, &mut canvas)?;
         canvas.finish(ctx)?;
 
         Ok(())
     }
 
-    fn key_down_event(
-        &mut self,
-        _ctx: &mut Context,
-        input: ggez::input::keyboard::KeyInput,
-        _repeated: bool,
-    ) -> Result<(), ggez::GameError> {
-        if let Some(key) = input.keycode.and_then(Direction::from_keycode) {
-            // print!("key pressed: {:?}", key);
-            // if let Some(direction) = key {
-            self.rectangle.direction = key;
-            // }
-            // match key {
-            //     Direction::Up => self.rectangle.y -= 1.0,
-            //     Direction::Down => self.rectangle.y += 1.0,
-            //     Direction::Right => self.rectangle.x += 1.0,
-            //     Direction::Left => self.rectangle.x -= 1.0
-            // }
-        };
-        Ok(())
-    }
+    // fn key_down_event(
+    //     &mut self,
+    //     _ctx: &mut Context,
+    //     input: KeyInput,
+    //     _repeated: bool,
+    // ) -> Result<(), ggez::GameError> {
+    //     if let Some(key) = input.keycode.and_then(Direction::from_keycode) {
+    //         self.rectangle.direction = key;
+    //     };
+    //     Ok(())
+    // }
 }
 
 fn main() -> GameResult {
@@ -230,10 +245,9 @@ fn main() -> GameResult {
         // Next we set up the window. This title will be displayed in the title bar of the window.
         .window_setup(ggez::conf::WindowSetup::default().title("My game!"))
         // Now we get to set the size of the window, which we use our SCREEN_SIZE constant from earlier to help with
-        .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0*2., SCREEN_SIZE.1*2.))
-        // .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
-        // .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
-        // .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
+        .window_mode(
+            ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0 * 2., SCREEN_SIZE.1 * 2.),
+        )
         // And finally we attempt to build the context and create the window. If it fails, we panic with the message
         // "Failed to build ggez context"
         .build()?;
